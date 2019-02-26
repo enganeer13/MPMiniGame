b7a1138c2968c197c8d7fe75fbdba72031b5e2b1
Josh Coyco <jac6308@gmail.com>
2019-02-26 14:00:29 -0800

    Added random level generation and test presets

---
+++ MPMiniGame/Assets/LevelGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject[] presets;
+    public int preset_width = 18;
+    public int levelLength = 10;
+
+    private void Start()
+    {
+        for(int i = 0; i < levelLength; i++)
+        {
+            GameObject preset = Instantiate(presets[Random.Range(0, presets.Length)], new Vector3(i * preset_width, 0, 0), Quaternion.identity);
+        }
+    }
+}
