3025843c407ed7dd16add43ad309e5e5adab15c3
Josh Coyco <jac6308@gmail.com>
2019-03-01 16:56:12 -0800

    Adjusted Level generation to work on differing level chunk lengths.

--- MPMiniGame/Assets/Scripts/LevelGenerator.cs
+++ MPMiniGame/Assets/Scripts/LevelGenerator.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour
 {
     public GameObject[] presets;
-    public int preset_width = 18;
     public int levelLength = 10;
 
-    private void Start()
+    IEnumerator Start()
     {
+        float location = 0f;
         for(int i = 0; i < levelLength; i++)
         {
-            GameObject preset = Instantiate(presets[Random.Range(0, presets.Length)], new Vector3(i * preset_width, 0, 0), Quaternion.identity);
+            GameObject presetToSpawn = presets[Random.Range(0, presets.Length)];
+            yield return presetToSpawn.GetComponent<LevelChunk>().Awake();
+            if (presetToSpawn.GetComponent<LevelChunk>())
+            {
+                if (i == 0)
+                {
+                    GameObject preset = Instantiate(presetToSpawn, new Vector3(location, 0, 0), Quaternion.identity);
+                    location += presetToSpawn.GetComponent<LevelChunk>().getWidth()/2;
+                }
+                else
+                {
+                    GameObject preset = Instantiate(presetToSpawn, new Vector3(location + presetToSpawn.GetComponent<LevelChunk>().getWidth()/2, 0, 0), Quaternion.identity);
+                    location += presetToSpawn.GetComponent<LevelChunk>().getWidth();
+                }
+                
+            }
+            else
+            {
+                Debug.Log("GameObject does not contain LevelChunk script.");
+            }
         }
     }
 }
