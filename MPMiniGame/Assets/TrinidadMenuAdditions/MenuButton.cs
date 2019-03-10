cf62e008f0246a20b809d25e517bf9ca02325614
Trinidad Maya <tjmaya@cpp.edu>
2019-03-10 13:44:09 -0700

    Settings Menu
    -Volume Settinng
    -Fullscreen toggle
    -Graphics Setting
    -Resolution Setting
    Added a Main Mixer that will be used for all sounds in the game. It is tied to the volume setting

--- MPMiniGame/Assets/TrinidadMenuAdditions/MenuButton.cs
+++ MPMiniGame/Assets/TrinidadMenuAdditions/MenuButton.cs
@@ -11,32 +11,43 @@ public class MenuButton : MonoBehaviour
     [SerializeField] int thisIndex;
     bool mouseClick;
     public int desiredSceneIndex;
+    float buttonDelay;
 
     // Update is called once per frame
     void Update()
     {
-        if(menuButtonController.index == thisIndex)
+        if (menuButtonController.index == thisIndex)
         {
             animator.SetBool("selected", true);
-            if (Input.GetAxis("Submit") == 1 || mouseClick == true) {
+            if (Input.GetAxis("Submit") == 1 || mouseClick == true)
+            {
                 animator.SetBool("pressed", true);
                 mouseClick = false;
-            } else if (animator.GetBool("pressed")) {
+            }
+            else if (animator.GetBool("pressed"))
+            {
                 animator.SetBool("pressed", false);
                 animatorFunctions.disableOnce = true;
-                if (desiredSceneIndex == 4)
-                {
-                    Debug.Log("quit app");
-                    //Application.Quit();
-                }
-                else
-                {
-                    Debug.Log("change scene");
-                    //SceneManager.LoadScene(desiredSceneIndex);
-                }
+                Invoke("changeScene", 0.4f);
+                
+                
             }
         } else {
             animator.SetBool("selected", false);
+        }
+    }
+
+    public void changeScene()
+    {
+        if (desiredSceneIndex == 4)
+        {
+            Debug.Log("quit app");
+            Application.Quit();
+        }
+        else
+        {
+            Debug.Log("change scene");
+            SceneManager.LoadScene(desiredSceneIndex);
         }
     }
 
