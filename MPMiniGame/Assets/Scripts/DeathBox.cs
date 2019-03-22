0d440781e2937a7c729d1b2624e8de3d60adb5f4
Josh Coyco <jac6308@gmail.com>
2019-03-21 18:20:30 -0700

    Added Cannon trap that shoots stuff. Also added cannonball projectile. Added DeathBox script, should be used for the bounds of the camera and anything that kills.

---
+++ MPMiniGame/Assets/Scripts/DeathBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<playerMovement>())
+            die();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<playerMovement>())
+            die();
+    }
+
+    protected void die()
+    {
+        Debug.Log("DIE");
+    }
+}
