f55a003a6e150c7ee0dba3f892d49e73c9b2d436
Josh Coyco <jac6308@gmail.com>
2019-03-21 18:39:07 -0700

    Added toggle and cooldown to cannons

--- MPMiniGame/Assets/Scripts/Cannon.cs
+++ MPMiniGame/Assets/Scripts/Cannon.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Cannon : MonoBehaviour, Trap
 {
     public float force;
+    public float cooldownOffset;
+    public bool toggle;
     public string tag;
+    private bool firing;
+    private float timer;
     private Vector3 direction;
     // Start is called before the first frame update
     void Start()
@@ -21,6 +25,26 @@ public class Cannon : MonoBehaviour, Trap
     public void activate()
     {
         //Shoot projectile of tag in specified direction and force
+        if (!toggle && Time.time > timer)
+        {
+            fire();
+            timer = Time.time + cooldownOffset;
+        }
+        else if(!firing && toggle && Time.time > timer)
+        {
+            InvokeRepeating("fire", 0.00001f, cooldownOffset);
+            firing = true;
+        }
+        else if(firing && toggle && Time.time > timer)
+        {
+            CancelInvoke();
+            firing = false;
+        }
+        
+    }
+
+    void fire()
+    {
         GameObject projectile = ObjectPooler.Instance.SpawnFromPool(tag, transform.position, Quaternion.identity);
         projectile.GetComponent<Rigidbody2D>().AddForce(direction * force);
     }
