811d0e049e3ba15de5d03bc92e68877e928ce149
enganeer13 <zacharydeigert@gmail.com>
2019-04-22 17:32:18 -0700

    Merging Master, Removing all demos, Adding ability to connect to photon server.

---
+++ MPMiniGame/Assets/Scripts/Photon/PhotonLobby.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+
+	public static PhotonLobby lobby;
+
+    public GameObject battleButton; //The button that has the player find a room to play in
+    public GameObject cancelButton;
+
+    private void Awake()
+    {
+        lobby = this; //creates a singleton, lives within the emain menu scene
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); //Connects to Master photon server
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player has connected to the Photon master server");
+        battleButton.SetActive(true);
+    }
+
+    public void OnBattleButtonClicked()
+    {
+        Debug.Log("Battle Button was clicked");
+        battleButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom(); //Attempts to join a random room
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to join a random game but failed. There must be no open games available");
+        CreateRoom();
+        //base.OnJoinRandomFailed(returnCode, message);
+    }
+
+    void CreateRoom()
+    {
+        Debug.Log("Trying to create a new Room");
+        int randomRoomName = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 4 };
+        PhotonNetwork.CreateRoom("Room" + randomRoomName, roomOps);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to create a new room but failed. There must be a room with the same name");
+        CreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("We are now in a room");
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        Debug.Log("Cancel Button was clicked");
+        cancelButton.SetActive(false);
+        battleButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+}
