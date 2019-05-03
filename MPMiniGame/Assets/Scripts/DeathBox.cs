7b84ea3097a870601d46d2b9dc3e5984b42a1c79
Trinidad Maya <tjmaya@cpp.edu>
2019-05-02 19:40:49 -0700

    Added a Death/RoundOver message and Added death box on both ends of the camera
    -Changed the countdown script to only manage the UI texts and added a RoundOver script that displays a death/roundover message.
    -Created a GameManager Script that handles the timings of the game and calls on the Countdown and RoundOver scripts to display their message.
    -Added a death box to both end of the camera in the PlayTest scene
    -Edited the deathbox and cannonball script to properly call on the roundEnd method in the GameManager script

--- MPMiniGame/Assets/Scripts/DeathBox.cs
+++ MPMiniGame/Assets/Scripts/DeathBox.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class DeathBox : MonoBehaviour
 {
+    GameObject gameManager;
+    GameManagement gameManagement;
+
+
+    private void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameController");
+        gameManagement = gameManager.GetComponent<GameManagement>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<playerMovement>())
-            die();
+        {
+            die(collision.gameObject);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<playerMovement>())
-            die();
+        {
+            die(collision.gameObject);
+        }
     }
 
-    protected void die()
+    protected void die(GameObject runner)
     {
         Debug.Log("DIE");
+        runner.SetActive(false);
+        gameManagement.endRound();
     }
 }
---
+++ MPMiniGame/Assets/Scripts/GameManagement.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagement : MonoBehaviour
+{
+    GameObject mainCamera;
+    scroll scroll;
+
+    GameObject[] players;
+    playerMovement[] playerMovement;
+
+    GameObject trapMaster;
+    TrapMaster trapMasterControl;
+
+    GameObject countDown;
+    Countdown countDownText;
+
+    GameObject roundOver;
+    RoundOver roundOverText;
+
+    float timer = 3.0f;
+    bool inCountdown = true;
+    bool roundIsOver = false;
+
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        scroll = mainCamera.GetComponent<scroll>();
+
+        players = GameObject.FindGameObjectsWithTag("Player");
+        playerMovement = new playerMovement[players.Length];
+        for (int i = 0; i < players.Length; i++)
+        {
+            playerMovement[i] = players[i].GetComponent<playerMovement>();
+        }
+
+        trapMaster = GameObject.FindGameObjectWithTag("TrapMaster");
+        trapMasterControl = trapMaster.GetComponent<TrapMaster>();
+
+        countDown = GameObject.Find("Countdown");
+        countDownText = countDown.GetComponent<Countdown>();
+
+        roundOver = GameObject.Find("RoundOver");
+        roundOverText = roundOver.GetComponent<RoundOver>();
+
+    }
+
+
+    void Update()
+    {
+        timer -= 1 * Time.deltaTime;
+        double count = Math.Ceiling(timer);
+
+        if(inCountdown == true)
+        {
+            if (timer > 0)
+            {
+                countDownText.countdownMessage(count.ToString());
+            }
+            else
+            {
+                countDownText.countdownMessage("Start!");
+                scroll.enabled = true;
+
+                for (int i = 0; i < playerMovement.Length; i++)
+                {
+                    if (playerMovement[i] != null)
+                    {
+                        playerMovement[i].enabled = true;
+                    }
+                }
+
+                trapMasterControl.enabled = true;
+            }
+            if (timer <= -1)
+            {
+                countDownText.countdownMessage("");
+                inCountdown = false;
+            }
+        }
+
+        if (timer <= 0 && roundIsOver == true)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void endRound()
+    {
+        roundOverText.endRoundMessage();
+        scroll.enabled = false;
+        trapMasterControl.enabled = false;
+        timer = 3;
+        roundIsOver = true;
+    }
+}
