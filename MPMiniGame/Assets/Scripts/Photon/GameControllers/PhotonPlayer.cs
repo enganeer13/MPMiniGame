e1c683eb3d55f233a5fca4b7f3ea3d6757214a27
enganeer13 <zacharydeigert@gmail.com>
2019-05-25 11:15:59 -0700

    Multiplayer features that aren't working: Animation Syncing Random Map generation Death Sync Trapmaster Traps

--- MPMiniGame/Assets/Scripts/Photon/GameControllers/PhotonPlayer.cs
+++ MPMiniGame/Assets/Scripts/Photon/GameControllers/PhotonPlayer.cs
@@ -48,7 +48,7 @@ public class PhotonPlayer : MonoBehaviour
                 int spawnPicker = Random.Range(0, GameSetup.GS.spawnPoints.Length);
                 if (PV.IsMine)
                 {
-                    myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Player 1"),
+                    myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Robit"),
                                                          GameSetup.GS.spawnPoints[spawnPicker].position, GameSetup.GS.spawnPoints[spawnPicker].rotation, 0);
                 }
             }
