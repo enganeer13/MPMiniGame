8bd8c8eb3190fb70e6f5c79044ded2bdea663779
enganeer13 <zacharydeigert@gmail.com>
2019-05-24 23:58:36 -0700

    Please work??

--- MPMiniGame/Assets/Scripts/Photon/GameControllers/PhotonPlayer.cs
+++ MPMiniGame/Assets/Scripts/Photon/GameControllers/PhotonPlayer.cs
@@ -38,7 +38,7 @@ public class PhotonPlayer : MonoBehaviour
                 int spawnPicker = Random.Range(0, GameSetup.GS.spawnPoints.Length);
                 if (PV.IsMine)
                 {
-                    myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Player 1"),
+                    myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "TrapMaster"),
                                                          GameSetup.GS.spawnPoints[spawnPicker].position, GameSetup.GS.spawnPoints[spawnPicker].rotation, 0);
                 }
             }
